1fbe8039ee0be8a6b320ec0ffd777a313878caec
Aidz <adrian.g.ngo@gmail.com>
2020-01-10 23:40:38 +0800

    change to localhost

--- frontend/src/_config/axios.js
+++ frontend/src/_config/axios.js
@@ -1,23 +1,7 @@
 import axios from "axios";
 
-// const instance = axios.create({
-//     //baseURL: 'http://localhost:3001'
-//     baseURL: 'https://d081df88.ngrok.io'
-// });
-
-// instance.interceptors.request.use(
-//     config => {
-//         const token = localStorage.getItem('adnatSessionId');
-//         if (token) {
-//             console.log("token = ", token);
-//             config.headers.Authorization = 'Bearer ' + token;
-//         }
-//         return config;
-//     }
-// )
-
 const adapter = axios.create({
-    baseURL: 'https://602ef4fb.ngrok.io',
+    baseURL: 'http://localhost:3001',
     headers: {
         Authorization: localStorage.getItem('adnatSessionId'),
         "Content-Type": "application/json"
